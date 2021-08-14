4131e912ff42cf07acdc821daf0b602774344f03
Marco-Ray <wangzehuan2017@sina.com>
2021-08-14 13:35:43 +0800

    init

---
+++ src/components/Content.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="bg">
+    This is the area of main content
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content'
+};
+</script>
+
+<style lang="scss" scoped>
+#bg {
+  width: 100%;
+  height: 100vh;
+  background-image: url('../assets/bg.jpg');
+  background-position: top left;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
