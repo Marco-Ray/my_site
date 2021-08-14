a40e8ee3267b4798c41082ecc8371879e6ede3f3
Marco-Ray <wangzehuan2017@sina.com>
2021-08-14 16:26:42 +0800

    home page complete

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,6 +1,18 @@
 <template>
-  <div id="bg">
-    This is the area of main content
+  <div id="bg" class="px-20 flex flex-col justify-center items-start">
+    <div class="px-3 py-10 bg-gray-500 bg-opacity-50">
+      <div class="text-7xl text-white flex flex-col lg:flex-row lg:gap-4">
+        <div>
+          <p >Hi! I'm</p>
+        </div>
+        <div>
+          <p><span class="text-red-500"> Zehuan</span> Wang</p>
+        </div>
+      </div>
+      <p class="text-white py-5 max-w-prose">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+        praesentium voluptatum deleniti atque corrupti quos dolores et quas
+        molestias excepturi sint occaecati cupiditate non provident.</p>
+    </div>
   </div>
 </template>
 
