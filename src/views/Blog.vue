3e95f6c86c4d34aea899fb960f0789be7bd1c1a7
Marco-Ray <wangzehuan2017@sina.com>
2021-08-17 17:50:21 +0800

    update the css style of markdown page add backToTop function

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="blog h-screen text-white text-7xl">
+  <div class="blog h-screen text-red-600 text-6xl">
     <router-link to="/">
-      <i class="fa fa-times absolute right-0"></i>
+      <i class="fa fa-times absolute right-5" />
     </router-link>
+    <i v-show="!isTop"
+      class="fa fa-arrow-up fixed bottom-5 right-5 z-10 cursor-pointer"
+      @click.prevent="backToTop"
+    />
     <mark-down-page></mark-down-page>
   </div>
 </template>
@@ -11,12 +15,29 @@
 import MarkDownPage from '@/components/MarkDownPage';
 export default {
   name: 'Blog',
-  components: { MarkDownPage }
+  components: { MarkDownPage },
+  data() {
+    return {
+      isTop: true,
+    }
+  },
+  methods: {
+    handleScroll() {
+      const { scrollTop } = document.documentElement;
+      this.isTop = scrollTop === 0;
+    },
+    backToTop() {
+      document.documentElement.scrollTop = 0;
+    }
+  },
+  created() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
 };
 </script>
 
 <style scoped>
-div {
-  color: red
-}
 </style>
